1eef25e2bcd73843ea40ebb0cc9c838e70478a4a
Xiao Xue <anxxue@gmail.com>
2024-01-09 16:12:03 +0100

    1st exercises

---
+++ JS/Basic Algorithms/Ejercicios.js
@@ -0,0 +1,20 @@
+/*1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.*/
+
+//!-------------------------------------------------------------
+//1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+let myFavoriteHero = "Hulk"
+//!-------------------------------------------------------------
+//1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+let x = 50
+//?-------------------------------------------------------------
+//1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+let h = 5
+let y = 10
+//!-------------------------------------------------------------
+//1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.*/
+let z = "h + y"
+
+
