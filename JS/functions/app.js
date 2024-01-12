0447a10fe054fae52a0308b5fa990145e3e58b1e
Xiao Xue <anxxue@gmail.com>
2024-01-12 23:45:53 +0100

    New files

---
+++ JS/functions/app.js
@@ -0,0 +1,29 @@
+//**Iteración #1: Buscar el máximo**
+
+//?---------------------------------------------------------------------------------------------
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+//?---------------------------------------------------------------------------------------------
+
+function sum(numberOne, numberTwo) {
+  return;
+}
+
+//?---------------------------------------------------------------------------------------------
+/**Iteración #2: Buscar la palabra más larga**
+
+Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función:*/
+//?---------------------------------------------------------------------------------------------
+
+const avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(param) {
+  // insert code
+}
