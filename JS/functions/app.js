43ae55496cd2ef5c53e5cdb67038548d81ab9b9d
Xiao Xue <anxxue@gmail.com>
2024-01-14 23:53:37 +0100

    Exercises functions sl

--- JS/functions/app.js
+++ JS/functions/app.js
@@ -5,8 +5,15 @@
 //?---------------------------------------------------------------------------------------------
 
 function sum(numberOne, numberTwo) {
-  return;
+  if (numberOne < numberTwo) {
+    return numberTwo;
+  } else {
+    return numberOne;
+  }
 }
+
+const result = sum(55, 49);
+console.log(result);
 
 //?---------------------------------------------------------------------------------------------
 /**Iteración #2: Buscar la palabra más larga**
@@ -24,6 +31,40 @@ const avengers = [
   "Spiderman",
   "Captain M.",
 ];
-function findLongestWord(param) {
+
+const findLongestWord = (arrayDeAvengers) => {
+  let longestAvenger = 0;
+  for (let i = 0; i < arrayDeAvengers.length; i++) {}
+};
+
+findLongestWord(avengers);
+
+// NO ME SALE.
+
+//?---------------------------------------------------------------------------------------------
+
+/**Iteración #3: Calcular la suma**
+
+Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz.
+
+Puedes usar este array para probar tu función:*/
+//?---------------------------------------------------------------------------------------------
+
+const numbers1 = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
   // insert code
 }
+
+//?---------------------------------------------------------------------------------------------
+/**Iteración #4: Calcular el promedio**
+
+Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:*/
+
+const numbers2 = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
+  // insert code
+}
+
+//?---------------------------------------------------------------------------------------------
