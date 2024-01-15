8e2bfe2959f21376fed6c6a50d8dd3fe27f89b2c
Xiao Xue <anxxue@gmail.com>
2024-01-15 23:09:36 +0100

    All loop exercises completed

--- JS/basicAlgorithms/6-bucles.js
+++ JS/basicAlgorithms/6-bucles.js
@@ -3,7 +3,7 @@
 //?---------------------------------------------------------------------------------------------
 // 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
 //?---------------------------------------------------------------------------------------------
-
+console.log("Task 1.1:");
 for (let loop = 0; loop <= 9; loop++) {
   console.log(loop);
 }
@@ -12,17 +12,24 @@ for (let loop = 0; loop <= 9; loop++) {
 // 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo
 // cuando el resto del numero dividido entre 2 sea 0.
 //?---------------------------------------------------------------------------------------------
-
-for (let loop = 0; loop <= 9; loop++) {
-  if (loop / 2 == 0) {
-    console.log(loop);
+console.log("Task 1.2:");
+for (let i = 0; i <= 9; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
   }
 }
-// NO ME SALE.
+
 //?---------------------------------------------------------------------------------------------
 // 1.3 Crea un bucle para conseguir dormir contando ovejas.
 // Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
 // Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle
 // y cambia el mensaje en la décima vuelta a 'Dormido!'.
 
-//working on this one.
+console.log("Task 1.3:");
+for (let i = 1; i <= 10; i++) {
+  if (i === 10) {
+    console.log(`Dormido! ${i}`);
+  } else {
+    console.log(`Intentando dormir 🐑 ${i}`);
+  }
+}
