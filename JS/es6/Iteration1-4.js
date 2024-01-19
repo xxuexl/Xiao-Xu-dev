e8a7903166194abd624e0bec1ffdc0f0c3f3c755
Xiao Xue <anxxue@gmail.com>
2024-01-19 23:19:19 +0100

    Editing

--- JS/es6/Iteration1-4.js
+++ JS/es6/Iteration1-4.js
@@ -206,3 +206,28 @@ con los valores que sean par.*/
 console.log("Task 5.2:");
 
 const agess = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+//?---------------------------------------------------------------------------------------------------
+/*5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array
+con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers = [
+//?---------------------------------------------------------------------------------------------------
+
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+//?---------------------------------------------------------------------------------------------------
+/*5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array
+con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos
+usar la funcion .includes() para la comprobación.*/
+//?---------------------------------------------------------------------------------------------------
+
+const streamers = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
