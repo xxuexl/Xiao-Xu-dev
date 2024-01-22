93db09405fd58536b391dfb9a4402c3150cd6713
Xiao Xue <anxxue@gmail.com>
2024-01-22 17:07:21 +0100

    Ejercicios hasta Iteración 3 corregidos

--- JS/basicAlgorithms/1-2-variables.js
+++ JS/basicAlgorithms/1-2-variables.js
@@ -18,14 +18,15 @@ let y = 10;
 //todo-----------------------------------------------------------------------------------------
 //1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.*/
 //todo-----------------------------------------------------------------------------------------
-let z = "h + y";
-console.log(z);
+let z = h + y;
 
 /*Iteración #2: Variables avanzadas*/
 //!---------------------------------------------------------------------------------------------
 /*1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 const character = {name: 'Jack Sparrow', age: 10};*/
 //!---------------------------------------------------------------------------------------------
+console.log("Task 2.1:");
+
 const character = { name: "Jack Sparrow", age: 10 };
 character.age = 25;
 console.log(character);
@@ -38,24 +39,36 @@ console.log(character);
 Muestralos por consola de esta forma: 
 	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'*/
 //?-------------------------------------------------------------------------------------------
+console.log("Task 2.2:");
+
 let firstName = " Jon ";
 let lastName = "Snow ";
 let age = 24;
+
 console.log(
-  "Soy" + firstName + lastName + "tengo 24 años y me gustan los lobos."
+  `Soy ${firstName}${lastName}, tengo ${age} años y me gustan los lobos.`
 );
 
+// originally
+console.log(
+  "Soy" + firstName + lastName + "tengo " + age + " años y me gustan los lobos."
+);
 //todo------------------------------------------------------------------------------------------
 /*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 ambos juguetes.
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};*/
 //todo------------------------------------------------------------------------------------------
+console.log("Task 2.3:");
+
 const toy1 = { name: "Buss myYear", price: 19 };
 const toy2 = { name: "Rallo mcKing", price: 29 };
 let result = toy1.price + toy2.price;
 console.log(result);
 
+//or
+
+console.log("Total Price", toy1.price + toy2.price);
 //!---------------------------------------------------------------------------------------------
 /*1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad
@@ -64,13 +77,24 @@ let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};*/
 //!---------------------------------------------------------------------------------------------
+console.log("Task 2.4:");
+
 let globalBasePrice = 10000;
 const car1 = { name: "BMW m&m", basePrice: 50000, finalPrice: 60000 };
 const car2 = { name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000 };
 
-globalBasePrice += 15000;
+globalBasePrice = 25000;
+console.log(globalBasePrice);
+car1.finalPrice = car1.basePrice + globalBasePrice;
+console.log("modified", car1);
+car2.finalPrice = car2.basePrice + globalBasePrice;
+console.log("modified", car2),
+  // originally I did:
+  (globalBasePrice = 25000);
 console.log(globalBasePrice);
 let car1result = car1.basePrice + globalBasePrice;
+console.log("modificado", car1result);
 let car2result = car2.basePrice + globalBasePrice;
-console.log(car1result);
-console.log(car2result);
+console.log("modificado", car2result);
+
+//ALL CORRECTED!
--- JS/basicAlgorithms/3-operadores.js
+++ JS/basicAlgorithms/3-operadores.js
@@ -2,6 +2,12 @@
 /*Iteración #3: Operadores*/
 /*1.1 Multiplica 10 por 5 y muestra el resultado mediante console.*/
 //?---------------------------------------------------------------------------------------------
+console.log("Task 1.1:");
+
+let multiply = 10 * 5;
+console.log("Resultado de multiplicar: " + multiply);
+
+//Original
 let a = 10;
 a *= 5;
 console.log(a);
@@ -9,7 +15,12 @@ console.log(a);
 //todo------------------------------------------------------------------------------------------
 /*1.2 Divide 10 por 2 y muestra el resultado en un console.*/
 //todo------------------------------------------------------------------------------------------
+console.log("Task 1.2:");
 
+let dividir = 10 / 2;
+console.log("Resultado de dividir: " + dividir);
+
+//Original
 let b = 10;
 b /= 2;
 console.log(b);
@@ -17,25 +28,32 @@ console.log(b);
 //!---------------------------------------------------------------------------------------------
 /*1.3 Muestra mediante un console el resto de dividir 15 por 9.*/
 //!---------------------------------------------------------------------------------------------
+console.log("Task 1.3:");
 
-let third = 15;
-third /= 9;
-console.log(third);
+let resto = 15 % 9;
+console.log("Resultado del resto de dividir " + resto);
 
 //?---------------------------------------------------------------------------------------------
 /*1.4 Usa el correcto operador de asignación que resultará en o = 15,
 teniendo dos variables p = 10 y j = 5.*/
 //?---------------------------------------------------------------------------------------------
+console.log("Task 1.4:");
+
 let p = 10;
 let j = 5;
 let o = p + j;
-console.log(o);
+console.log(o); //Original
+console.log("o vale el valor: " + o);
 
 //todo------------------------------------------------------------------------------------------
 /*1.5 Usa el correcto operador de asignación que resultará en i = 50,
 teniendo dos variables c = 10 y m = 5.*/
 //todo------------------------------------------------------------------------------------------
+console.log("Task 1.5:");
+
 let c = 10;
 let m = 5;
 let i = c * m;
-console.log(i);
+console.log("i vale: " + i);
+
+//EJERCICIOS CORREGIDOS!
