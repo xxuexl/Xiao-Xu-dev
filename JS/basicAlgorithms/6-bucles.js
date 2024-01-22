023c1c1bc422a19263f1397afea66e6a7ba7f8d7
Xiao Xue <anxxue@gmail.com>
2024-01-22 17:18:45 +0100

    Corregido bucles BA

--- JS/basicAlgorithms/6-bucles.js
+++ JS/basicAlgorithms/6-bucles.js
@@ -4,6 +4,11 @@
 // 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
 //?---------------------------------------------------------------------------------------------
 console.log("Task 1.1:");
+for (let x = 0; x < 10; x++) {
+  console.log(x);
+}
+
+// Original
 for (let loop = 0; loop <= 9; loop++) {
   console.log(loop);
 }
@@ -13,19 +18,36 @@ for (let loop = 0; loop <= 9; loop++) {
 // cuando el resto del numero dividido entre 2 sea 0.
 //?---------------------------------------------------------------------------------------------
 console.log("Task 1.2:");
+for (let x = 0; x < 10; x++) {
+  if (x % 2 == 0) {
+    console.log(x);
+  }
+}
+
+// Original
 for (let i = 0; i <= 9; i++) {
   if (i % 2 === 0) {
     console.log(i);
   }
 }
 
-//?---------------------------------------------------------------------------------------------
+//?-----------------------------------------------------------------------------------------------
 // 1.3 Crea un bucle para conseguir dormir contando ovejas.
 // Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
 // Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle
 // y cambia el mensaje en la décima vuelta a 'Dormido!'.
 
+//?-----------------------------------------------------------------------------------------------
 console.log("Task 1.3:");
+for (let x = 0; x < 11; x++) {
+  if (x == 10) {
+    console.log("Dormido!");
+  } else {
+    console.log("Intentando dormir, vuelta numero 🐑" + (x + 1));
+  }
+}
+
+// Original
 for (let i = 1; i <= 10; i++) {
   if (i === 10) {
     console.log(`Dormido! ${i}`);
@@ -33,3 +55,5 @@ for (let i = 1; i <= 10; i++) {
     console.log(`Intentando dormir 🐑 ${i}`);
   }
 }
+
+//EJERCICIOS CORREGIDOS!
