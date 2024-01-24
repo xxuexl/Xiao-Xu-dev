8d993bbb2f93d873a6f82645aeeabccc847a6718
Xiao Xue <anxxue@gmail.com>
2024-01-24 19:21:26 +0100

    Events exercises 1-2

---
+++ HTML/3-Js events/app.js
@@ -0,0 +1,11 @@
+//1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el
+//evento click que ejecute un console log con la información del evento del click
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("btnToClick").addEventListener("click", (e) => {
+    console.log(e);
+  });
+});
+
+//1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+btnToClick.focus();
+//1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
