e113ed210a6a6def49395d19217d8e82f3cbc871
Xiao Xue <anxxue@gmail.com>
2024-01-24 19:46:19 +0100

    Exercise 1.3

--- HTML/3-Js events/app.js
+++ HTML/3-Js events/app.js
@@ -4,8 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("btnToClick").addEventListener("click", (e) => {
     console.log(e);
   });
+  btnToClick.focus();
 });
 
 //1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
-btnToClick.focus();
+
 //1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("input").addEventListener("click", (e) => {
+    console.log(e);
+  });
+});
