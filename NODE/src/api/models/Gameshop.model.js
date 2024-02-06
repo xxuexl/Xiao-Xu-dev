902d64f6d8eef2c0e1eb2b71a2f77ef2e69582e6
Xiao Xue <anxxue@gmail.com>
2024-02-06 22:40:38 +0100

    Created product model

--- NODE/src/api/models/Gameshop.model.js
+++ NODE/src/api/models/Gameshop.model.js
@@ -6,14 +6,14 @@ Es una colección lógica organizada de objetos de base de datos como
 tablas, funciones...*/
 
 /* Creamos un nuevo Schema de los productos que venden las tiendas.
-Creamos un nuevo Schema de empresas/tiendas que venden productos
-(principalmente juegos) de la librería mongoose que interactúa con Mongo DB. 
 
-Creamos una nueva función a la que le pasamos un objeto con diferentes 
-claves y atributos
-*/
+Creamos un nuevo Schema de empresas/tiendas que venden productos
+(principalmente juegos) con la librería mongoose que interactúa con Mongo DB. 
+Tiene una nueva función a la que le pasamos un objeto con diferentes 
+claves y atributos. */
+
 const ProductSchema = new mongoose.Schema({ name: String });
-//Define
+//Define el esquema del producto, a través de un array String.
 
 const GameShopSchema = new mongoose.Schema(
   {
---
+++ NODE/src/api/models/Product.models.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+//const cloudinary = require("cloudinary");
+
+//Define el esquema de la plataforma del juego, a través de un array String.
+const PlatformSchema = new mongoose.Schema({ name: String });
+
+/* Creo un nuevo Schema del producto(videojuego) con la librería mongoose 
+que interactúa con Mongo DB. */
+const ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    genre: { type: String, required: true },
+    platforms: [PlatformSchema], //El array que representa en cuántas platforms está el juego.
+    price: { type: Number, required: true },
+    game_studio: { type: String, required: true },
+    year_released: { type: Number, required: true },
+    favourite: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/* El model es una representación estructurada de los datos que se 
+utilizan. Crea un modelo basado en el esquema definido en la parte 
+superior
+GameShop es el modelo creado a partir de este esquema, con él se
+pueden realizar operaciones CRUD. En paréntesis introducimos
+el nombre del modelo dentro de mongoose y luego hacemos referencia
+al esquema*/
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
